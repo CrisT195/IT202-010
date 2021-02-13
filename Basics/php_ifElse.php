2d120934df970b3195c4d90f3afc16394cab9c8f
CrisT195 <cth9@njit.edu>
2021-02-13 14:12:08 -0500

    Added more PHP basics

---
+++ Basics/php_ifElse.php
@@ -0,0 +1,31 @@
+<?php
+echo "<br>\nPHP Exercise If...Else Exercise 1<br>\n";
+$a = 50;
+$b = 10;
+if($a > $b) {
+	echo "Hello World";
+}
+
+echo "<br><br>\nPHP Exercise If...Else Exercise 2<br>\n";
+if($a != $b) {
+	echo "Hello World";
+}
+
+echo "<br><br>\nPHP Exercise If...Else Exercise 3<br>\n";
+if ($a == $b) {
+	echo "Yes";
+} else {
+	echo "No";
+}
+
+echo "<br><br>\nPHP Exercise If...Else Exercise 4<br>\n";
+if ($a == $b) {
+	echo "1";
+} elseif ($a > $b){
+	echo "2";
+} else {
+	echo "3";
+}
+
+
+?>
